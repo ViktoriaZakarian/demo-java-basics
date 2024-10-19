9afcfcde8fbeec7b2f4826740e0b733ad6f7a63b
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-10-19 17:13:54 +0300

    add 10 methods makeNumberToString

---
+++ src/by/itacademy/viktoriazakarian/numbers/Main.java
@@ -0,0 +1,68 @@
+package by.itacademy.viktoriazakarian.numbers;
+
+public class Main {
+    public static void main(String[] args) {
+        int number = 1;
+        String numberAsNumber = "";
+    }
+
+    public void makeOneToString(int num) {
+        if (num == 1) {
+            System.out.println("Один");
+        }
+    }
+
+    public void makeTwoToString(int num) {
+        if (num == 2) {
+            System.out.println("Два");
+        }
+    }
+
+    public void makeThreeToString(int num) {
+        if (num == 3) {
+            System.out.println("Три");
+        }
+    }
+
+    public void makeFourToString(int num) {
+        if (num == 4) {
+            System.out.println("Четыре");
+        }
+    }
+
+    public void makeFiveToString(int num) {
+        if (num == 5) {
+            System.out.println("Пять");
+        }
+    }
+
+    public void makeSixToString(int num) {
+        if (num == 6) {
+            System.out.println("Шесть");
+        }
+    }
+
+    public void makeSevenToString(int num) {
+        if (num == 7) {
+            System.out.println("Семь");
+        }
+    }
+
+    public void makeEightToString(int num) {
+        if (num == 8) {
+            System.out.println("Восемь");
+        }
+    }
+
+    public void makeNineToString(int num) {
+        if (num == 9) {
+            System.out.println("Девять");
+        }
+    }
+
+    public void makeZeroToString(int num) {
+        if (num == 0) {
+            System.out.println("Ноль");
+        }
+    }
+}
