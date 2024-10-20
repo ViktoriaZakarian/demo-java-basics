c84e2a77bcfc5dcb3280413038d822c52caabf59
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-10-20 15:25:13 +0300

    refactor 10 methods into 1 method makeNumberToString

--- src/by/itacademy/viktoriazakarian/numbers/Main.java
+++ src/by/itacademy/viktoriazakarian/numbers/Main.java
@@ -2,74 +2,38 @@ package by.itacademy.viktoriazakarian.numbers;
 
 public class Main {
     public static void main(String[] args) {
-        int number = 9;
-        makeOneToString(number);
-        makeTwoToString(number);
-        makeThreeToString(number);
-        makeFourToString(number);
-        makeFiveToString(number);
-        makeSixToString(number);
-        makeSevenToString(number);
-        makeEightToString(number);
-        makeNineToString(number);
-        makeZeroToString(number);
+        int number = 5;
+        makeNumberToString(number);
     }
 
-    public static void makeOneToString(int num) {
+    public static void makeNumberToString(int num) {
         if (num == 1) {
             System.out.println("Один");
         }
-    }
-
-    public static void makeTwoToString(int num) {
         if (num == 2) {
             System.out.println("Два");
         }
-    }
-
-    public static void makeThreeToString(int num) {
         if (num == 3) {
             System.out.println("Три");
         }
-    }
-
-    public static void makeFourToString(int num) {
         if (num == 4) {
             System.out.println("Четыре");
         }
-    }
-
-    public static void makeFiveToString(int num) {
         if (num == 5) {
             System.out.println("Пять");
         }
-    }
-
-    public static void makeSixToString(int num) {
         if (num == 6) {
             System.out.println("Шесть");
         }
-    }
-
-    public static void makeSevenToString(int num) {
         if (num == 7) {
             System.out.println("Семь");
         }
-    }
-
-    public static void makeEightToString(int num) {
         if (num == 8) {
             System.out.println("Восемь");
         }
-    }
-
-    public static void makeNineToString(int num) {
         if (num == 9) {
             System.out.println("Девять");
         }
-    }
-
-    public static void makeZeroToString(int num) {
         if (num == 0) {
             System.out.println("Ноль");
         }
